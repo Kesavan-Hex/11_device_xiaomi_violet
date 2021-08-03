48b6070caba9aa8d1572993d4a6269d9a6162d74
KesavanG <73026586+Kesavan-Hex@users.noreply.github.com>
2021-08-03 19:47:46 +0530

    Violet:Updated August Redfin FP RQ3A.210805.001.A1

--- revengeos_violet.mk
+++ revengeos_violet.mk
@@ -23,8 +23,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="violet"
        
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210705.001 7380771 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" \
